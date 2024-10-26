47c62dde30f6b447e9e3e9c8ee83e8854d3016e6
usamaamjad811 <usamaamjad811@gmail.com>
2024-10-26 16:13:36 +0500

    Main code updated

--- app/main.py
+++ app/main.py
@@ -7,31 +7,49 @@ from app.crud import (
 
 app = FastAPI()
 
+
 @app.get("/articles/")
 async def read_articles():
-    return await get_all_articles()
+    articles = await get_all_articles()
+    return {
+        "message": "Articles retrieved successfully",
+        "data": articles
+    }
 
 @app.get("/articles/{id}")
 async def read_article(id: str):
     article = await get_article_by_id(id)
     if article is None:
         raise HTTPException(status_code=404, detail="Article not found")
-    return article
+    return {
+        "message": "Article retrieved successfully",
+        "data": article
+    }
 
 @app.post("/articles/")
 async def create_new_article(article: Article):
-    return await create_article(article)
+    new_article = await create_article(article)
+    return {
+        "message": "Article created successfully",
+        "data": new_article
+    }
 
 @app.put("/articles/{id}")
 async def update_existing_article(id: str, article: Article):
     updated_article = await update_article(id, article)
     if updated_article is None:
         raise HTTPException(status_code=404, detail="Article not found")
-    return updated_article
+    return {
+        "message": "Article updated successfully",
+        "data": updated_article
+    }
 
 @app.delete("/articles/{id}")
 async def delete_existing_article(id: str):
     deleted_status = await delete_article(id)
     if deleted_status is None:
         raise HTTPException(status_code=404, detail="Article not found")
-    return deleted_status
+    return {
+        "message": "Article deleted successfully",
+        "data": deleted_status
+    }
