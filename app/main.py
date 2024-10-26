2c7052ba029a9c7afd94be857d1c77a6e1425f76
usamaamjad811 <usamaamjad811@gmail.com>
2024-10-26 16:31:14 +0500

    database and main code updated

--- app/main.py
+++ app/main.py
@@ -4,9 +4,27 @@ from app.schemas import Article
 from app.crud import (
     get_all_articles, get_article_by_id, create_article, update_article, delete_article
 )
+from fastapi.middleware.cors import CORSMiddleware
+import openai
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+@app.get("/")
+def read_root():
+    return {"message": "AI-ArticleManger APIs are working...."}
 
 @app.get("/articles/")
 async def read_articles():
